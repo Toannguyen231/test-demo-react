e0b60bc47ed77444c707f73f34d5b8e4e792f09d
Ngoc Toan <ngoctoann06@gmail.com>
2025-11-09 18:56:24 +0700

    update active link

--- src/component/Header/Nav.jsx
+++ src/component/Header/Nav.jsx
@@ -2,8 +2,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Link } from 'react-router-dom';
-function BasicExample() {
+import { NavLink } from 'react-router-dom';
+function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
@@ -11,9 +11,9 @@ function BasicExample() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Link to="/" className='nav-link'>Home</Link>
-                        <Link to="/user" className='nav-link'>User</Link>
-                        <Link to="/admin" className='nav-link'>Admin</Link>
+                        <NavLink to="/" className='nav-link'>Home</NavLink>
+                        <NavLink to="/user" className='nav-link'>User</NavLink>
+                        <NavLink to="/admin" className='nav-link'>Admin</NavLink>
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
@@ -35,4 +35,4 @@ function BasicExample() {
     );
 }
 
-export default BasicExample;
+export default Header;
